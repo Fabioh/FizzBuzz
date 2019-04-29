9774d1122a8b4e0c20390ea6816e1d1cf6154ed7
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2019-04-29 15:46:50 -0300

    - FizzBuzz implementation in c#

---
+++ src/FizzBuzz.Core/FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz.Core
+{
+    public class FizzBuzz
+    {
+        public static IEnumerable<string> GetFizzBuzz(int quantity) =>
+            Enumerable.Range(1, quantity)
+            .Select(n => (n % 3, n % 5) switch
+            {
+                (0, 0) => "FizzBuzz",
+                (0, _) => "Fizz",
+                (_, 0) => "Buzz",
+                (_, _) => n.ToString(),
+            });
+    }
+}
---
+++ tests/FizBuzz.Terminal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static FizzBuzz.Core.FizzBuzz;
+
+namespace FizBuzz.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetFizzBuzz(10)
+            int i = 1;
+            foreach (var item in GetFizzBuzz(20))
+            {
+                Console.WriteLine($"[{i++}] = {item}");
+            }
+        }
+    }
+}
